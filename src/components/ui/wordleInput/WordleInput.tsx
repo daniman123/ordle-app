80ab9f391d3c3e162bebf8308858fdc9a0fca681
daniman123 <daniel-saleh@hotmail.com>
2024-02-04 22:19:56 +0100

    style: wordleInput

--- src/components/ui/wordleInput/WordleInput.tsx
+++ src/components/ui/wordleInput/WordleInput.tsx
@@ -5,22 +5,17 @@ export interface IWordleInput {}
 const WordleInput = () => {
   const { handleChange, handleKeyDown, inputRefs, inputs } = useWordleInput();
   return (
-    <div className="WordleInput-container">
+    <div className="flex gap-2 justify-center">
       {inputs.map((_, index) => (
         <input
+          className="w-[50px] h-[50px] rounded-md text-center black"
           key={index}
           ref={inputRefs.current[index]}
           type="text"
+          maxLength={1}
           value={inputs[index]}
           onChange={(event) => handleChange(index, event)}
-          maxLength={1}
           onKeyDown={(e) => handleKeyDown(e, index)}
-          style={{
-            width: "40px",
-            marginRight: "5px",
-            textAlign: "center",
-            color: "black",
-          }}
         />
       ))}
     </div>
